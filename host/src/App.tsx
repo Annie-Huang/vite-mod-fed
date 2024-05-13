5efd442a016113a41ac19531e509680ce73f1110
Annie Huang <annie.hn@gmail.com>
2024-05-13 22:02:38 +1000

    Expose the store in vite.config.ts in remote and use it in <App> in host app so we can share state between two buttons in host app.

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
 
 // @ts-ignore
 import Button from 'remoteApp/Button';
 
+// You can change it to useStore because we "export default useCount;", you can call it any useXXX you like.
+// @ts-ignore
+import useStore from 'remoteApp/store';
+
 function App() {
-  const [count, setCount] = useState(0);
+  // The <button> count (Text with 'count is') is different from <Button> (Text with 'Click me') because they are from different state.
+  // const [count, setCount] = useState(0);
+
+  // This force both <button> and <Button> use the same useCount hook defined in C:\react\vite-mod-fed\remote\src\store.ts
+  // so the count value is consistent updated.
+  // "Note01 - Each app can shared state between own App button and remote app Button.jpg"
+  const [count, setCount] = useStore();
 
   return (
     <div className='App'>
       <h1>Host Application</h1>
       <Button />
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count: number) => count + 1)}>
           count is {count}
         </button>
       </div>
--- remote/src/store.ts
+++ remote/src/store.ts
@@ -5,6 +5,9 @@ const countAtom = atom(0);
 // We will have a custom hook called useCount, which will return a use atom that's going to give us back a value for it
 // as well as a setter in a tuple pretty much like useState. So that will be the default output of this store,
 // will be this useCount and that we can use for our count.
+//
+// And you also need to share the store between the remote application and the host application in vite.config.ts.
+// (and need to add the jotia library into the shared list as well)
 const useCount = () => useAtom(countAtom);
 
 export default useCount;
